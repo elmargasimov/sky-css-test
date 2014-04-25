94d6d72fe28c58e3e389ea5a9cd5e50555ba8749
Elmar Gasimov <e.s.gasimov@gmail.com>
2014-04-25 02:55:21 +0100

    add additional styling changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+$('#site-search').on('click', function(e) {
+    var $autoComplete = $('#toggle-auto-complete');
+
+    if($autoComplete.is(':visible')) {
+        $autoComplete.hide();
+    } else {
+        $autoComplete.show();
+    }
+});
